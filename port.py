b84f777f1193a1163b883a91dc66d942fe9d75e9
adriantora <adriantorax@gmail.com>
2017-10-26 22:36:36 -0500

    add web server option

--- port.py
+++ port.py
@@ -5,19 +5,47 @@ import pyqrcode
 import png
 import sys
 import os
+from  minserv import *
 
-if sys.argv[1] == "-f":
-    try:
-        f=open(sys.argv[2]).read()
-        url = pyqrcode.create(f)
-        url.png("url.png",scale=8)
-	print(url.terminal(quiet_zone=1))
-    except Exception as e:
-        print("archivo no encontrado.")
+#revisar sus usos para limpiar
+import socket
+import fcntl
+import struct
+
+def get_lan_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+    ip = s.getsockname()[0]
+
+    return ip
+
+
+def generador(text):
+    url = pyqrcode.create(text)
+    url.png("/tmp/image.png",scale=8)
+    print(url.terminal(quiet_zone=1))
+
+if len(sys.argv) == 1:
+    print("Write text: ")
+    texto=raw_input()
+    generador(texto)
 else:
-    try:
-        url = pyqrcode.create(sys.argv[1])
-        url.png("url.png",scale=8)
-	print(url.terminal(quiet_zone=1))
-    except:
-        print("No se a especificado la url.")
+
+    if len(sys.argv) > 0:
+        if sys.argv[1] != "--import" and sys.argv[1] != "-f":
+            generador(sys.argv[1])
+
+        if sys.argv[1] == "--import":
+            ip=get_lan_ip()if len(sys.argv) > 1:
+            port=5000
+            print ip+":"+str(port)
+            generador(ip+":"+str(port))
+            minserver(ip,port)#recibir la informacion de la funcion.
+    
+        #mostrador de archivos especificado con "-f"
+        if sys.argv[1] == "-f":
+            try:
+                f=open(sys.argv[2]).read()
+                generador(f)
+            except Exception as e:
+                print("File not found.")
